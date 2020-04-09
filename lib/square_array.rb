3124b9e1f8e4410ffa042dcc6deac7245a02ad46
Youngmin Park <codepark2019@gmail.com>
2020-04-09 07:09:32 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   count = 0
   new_array = []
   while array[count] do
-    new_array.push(array[count] * array[count])
+    new_array.push(array[count] ** 2)
     count += 1
   end
   new_array
